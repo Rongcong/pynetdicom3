d78169b8d64c939f2c47b610a27aa60697359d2b
xu rongcong <xrongcong@infervision.com>
2018-10-31 17:40:41 +0800

    Fix bugs.

--- pynetdicom3/utils.py
+++ pynetdicom3/utils.py
@@ -237,8 +237,9 @@ class PresentationContext(object):
                 raise ValueError('Presentation Context attempted to add a '
                                  'invalid UID')
             # Issue #62: private transfer syntaxes may be used
-            if not transfer_syntax.is_private and \
-                                not transfer_syntax.is_transfer_syntax:
+            # if not transfer_syntax.is_private and \
+            #                     not transfer_syntax.is_transfer_syntax:
+            if not transfer_syntax.is_transfer_syntax:
                 raise ValueError('Presentation Context attempted to add a '
                                  'non-transfer syntax UID')
             self.TransferSyntax.append(transfer_syntax)
